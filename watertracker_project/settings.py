5c02131c3cf0133dbfbb726fc60d880e3ad0f70e
Jeremy Gorniak <railspider@gmail.com>
2022-02-02 20:37:25 +1100

    Update settings.py

--- watertracker_project/settings.py
+++ watertracker_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+from local_settings import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eqs=4p+7yhb8fp=^0@r*z%)7l*kdp9!13p+r0+36zam1-&9wcz'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
